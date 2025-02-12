80174e16182f54fea22cabe3237e45749ba3e047
Vicente Casares <vicsares@gmail.com>
2025-02-12 09:00:02 +0100

    Módulo Custom

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Odoo14-PgAdmin
+
+### Modo de empleo
+```Bash
+docker-compose up -d
+```
+#### Odoo:
+http://localhost:8069
+#### PgAdmin:
+http://localhost:5050
---
+++ custom-addons/mi_modulo/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Mi Módulo Personalizado",
+    'version': '14.0.1.0.0',
+    'summary': "Descripción breve del módulo",
+    'description': "Descripción completa de lo que hace el módulo.",
+    'author': "Tu Nombre o Empresa",
+    'category': 'Custom',
+    'depends': ['base'],
+    'data': [
+        # Listado de archivos XML, CSV, etc.
+    ],
+    'installable': True,
+    'application': True,
+}
+
